f6522fc7448c60b80fd2d40e4410f3285a571fb6
Tony Woo <tonyswoo@ucdavis.edu>
2018-11-21 02:27:55 -0800

    Initial Project Setup

---
+++ Project/Project-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  TestSpotifyApp-Bridging-Header.h
+//  TestSpotifyApp
+//
+//  Created by user147183 on 11/18/18.
+//
+
+#ifndef TestSpotifyApp_Bridging_Header_h
+#define TestSpotifyApp_Bridging_Header_h
+
+#include <SpotifyiOS/SpotifyiOS.h>
+
+#endif /* TestSpotifyApp_Bridging_Header_h */
