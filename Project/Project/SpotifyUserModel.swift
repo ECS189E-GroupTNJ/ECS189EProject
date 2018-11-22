a3a3b0c4996cb42e836be63a1d46fdd993693009
Tony Woo <tonyswoo@ucdavis.edu>
2018-11-21 19:29:20 -0800

    Added functions for Spotify API calls

---
+++ Project/Project/SpotifyUserModel.swift
@@ -0,0 +1,82 @@
+//
+//  SpotifyUserModel.swift
+//  Project
+//
+//  Created by user147183 on 11/21/18.
+//  Copyright © 2018 groupTNJ. All rights reserved.
+//
+
+import Foundation
+
+class SpotifyUserModel {
+    
+    typealias ApiCompletion = ((_ response: [String: Any]?) -> Void)
+    
+    func configuration() -> URLSessionConfiguration {
+        let config = URLSessionConfiguration.ephemeral
+        config.timeoutIntervalForRequest = 10
+        config.timeoutIntervalForResource = 10
+        return config
+    }
+    
+    func SpotifyAPI(endpoint: String, param: [String: Any]?, completion: @escaping ApiCompletion) {
+        let delegate = UIApplication.shared.delegate as! AppDelegate
+        
+        // need to get proper access token, waiting for authentication process
+        guard let url = URL(string: endpoint), let accessToken = delegate.appRemote.connectionParameters.accessToken else {
+            return
+        }
+        
+        let session = URLSession(configuration: configuration())
+        var request = URLRequest(url: url)
+        request.setValue("Bearer \(accessToken)", forHTTPHeaderField: "Authorization")
+        
+        if let realParam = param {
+            request.httpMethod = "POST"
+            SpotifyPOST(session: session, request: request, param: realParam, completion: completion)
+        }
+        else {
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.httpMethod = "GET"
+            SpotifyGET(session: session, request: request, completion: completion)
+        }
+    }
+    
+    func SpotifyGET(session: URLSession, request: URLRequest, completion: @escaping ApiCompletion) {
+        session.dataTask(with: request) { (data, response, error) in
+            self.handleHTTPResponse(withData: data, response: response, error: error, completion: completion)
+            }.resume()
+    }
+    
+    func SpotifyPOST(session: URLSession, request: URLRequest, param: [String: Any], completion: @escaping ApiCompletion) {
+        guard let body = try? JSONSerialization.data(withJSONObject: param) else {
+            DispatchQueue.global(qos: .userInitiated).async { completion(nil) }
+            return
+        }
+        
+        session.uploadTask(with: request, from: body) { (data, response, error) in
+            self.handleHTTPResponse(withData: data, response: response, error: error, completion: completion)
+            }.resume()
+    }
+    
+    func handleHTTPResponse(withData data: Data?, response: URLResponse?, error: Error?, completion: @escaping ApiCompletion) {
+        if let httpResponse = response as? HTTPURLResponse {
+            if httpResponse.statusCode != 200 && httpResponse.statusCode != 201 {
+                DispatchQueue.global(qos: .userInitiated).async { completion(nil) }
+                return
+            }
+        }
+        guard let rawData = data else {
+            DispatchQueue.global(qos: .userInitiated).async { completion(nil) }
+            return
+        }
+        let json = try? JSONSerialization.jsonObject(with: rawData)
+        guard let responseData = json as? [String: Any] else {
+            DispatchQueue.global(qos: .userInitiated).async { completion(nil) }
+            return
+        }
+        DispatchQueue.global(qos: .userInitiated).async { completion(responseData) }
+    }
+    
+    
+}
