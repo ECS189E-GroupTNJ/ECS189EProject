e7bf4ad080b866b9c526fa74f070a7d02cb17168
noshin-k <noskamal@ucdavis.edu>
2018-11-24 21:16:20 -0800

    pod error

--- Project/Project/ViewController.swift
+++ Project/Project/ViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var connectWithSpotifyButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        connectWithSpotifyButton.layer.cornerRadius = connectWithSpotifyButton.frame.height / 2
+        connectWithSpotifyButton.setTitleColor(UIColor.white, for: .normal)
+        connectWithSpotifyButton.backgroundColor = UIColor.green
+        
+        connectWithSpotifyButton.layer.shadowColor = UIColor.white.cgColor
+        connectWithSpotifyButton.layer.shadowRadius = 50
+        connectWithSpotifyButton.layer.shadowOpacity = 0.5
+        connectWithSpotifyButton.layer.shadowOffset = CGSize(width: 0, height: 0)
+        
     }
-
 
 }
 
